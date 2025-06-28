362e69b658e31158c92647f885ae420eeb92bfd2
leonfirme1 <leon.firme@hotmail.com>
2025-06-28 11:21:24 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,3 @@
-
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -8,11 +7,11 @@ const PORT = process.env.PORT || 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const distPath = path.resolve(__dirname, "../dist/public");
+
+const distPath = path.resolve(__dirname, "../client/dist");
 
 app.use(express.static(distPath));
 
-// fallback para SPA
 app.get("*", (_req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
 });
